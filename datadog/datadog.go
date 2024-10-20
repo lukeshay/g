dd9bed4e307b2716e68b6dbe89546304f688ea46
Luke Shay <shay.luke17@gmail.com>
2024-10-20 11:56:57 -0500

    fix: set profiler agent address

--- datadog/datadog.go
+++ datadog/datadog.go
@@ -24,6 +24,7 @@ func Initialize(options IntializeOptions) func() error {
 			tracer.WithServiceVersion(options.DDVersion),
 			tracer.WithLogStartup(false),
 			tracer.WithDebugMode(false),
+			tracer.WithAgentAddr(fmt.Sprintf("%s:8126", options.DDAgentHost)),
 		)
 		profiler.Start(
 			profiler.WithEnv(options.DDEnv),
@@ -32,8 +33,13 @@ func Initialize(options IntializeOptions) func() error {
 			profiler.WithProfileTypes(
 				profiler.CPUProfile,
 				profiler.HeapProfile,
+				profiler.BlockProfile,
+				profiler.GoroutineProfile,
+				profiler.MetricsProfile,
+				profiler.MutexProfile,
 			),
 			profiler.WithLogStartup(false),
+			profiler.WithAgentAddr(fmt.Sprintf("%s:8126", options.DDAgentHost)),
 		)
 		statsd.New(fmt.Sprintf("http://%s:8125", options.DDAgentHost))
 	}
